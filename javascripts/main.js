7b3435d63c39d210f83f4795ac2f8a3115d191a3
PhAnToM <PhAnToM@iPhAnToMAir.local>
2015-05-23 13:15:00 +0800

    1) put my home page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+$(function() {
+
+$(document).ready(function() {
+	$('#toggleMenu').on('click', function(){
+		if($('#menu').hasClass('show') ){
+			$('#menu').removeClass('show');
+		} else{
+			$('#menu').addClass('show');	
+		}
+		
+	});
+	$('#menu .bg').on('click', function(){
+			$('#menu').removeClass('show');
+	});
+	$('#menu_list > li').on('click', function(e){
+		$('#menu_list .active').removeClass('active');
+		$(e.currentTarget).addClass('active');
+	});
+});
+
+
+});
