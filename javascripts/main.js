0ef82cbed85234ac8aecb38d2331ea087ccffc58
PhAnToM <PhAnToM@iPhAnToMAir.local>
2015-05-31 19:43:14 +0800

    i) my webpage version 1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,77 @@
 $(function() {
 
 $(document).ready(function() {
-	$('#toggleMenu').on('click', function(){
-		if($('#menu').hasClass('show') ){
-			$('#menu').removeClass('show');
-		} else{
-			$('#menu').addClass('show');	
+
+// Set Page UI
+	$('#HomePage').fullpage({
+		anchors: ['Main', 'codeworks','illustrworks'],
+		navigation: true,
+		navigationTooltips: ['About', 'Coding', 'Works']
+	});
+  $("#mainImg").twentytwenty();
+
+
+	
+	var web = Works.where({'type':'web'});
+	var illust = Works.where({'type':'illu'});
+	var workTmp = Handlebars.compile($('#workTmp').html());
+	web.forEach(function(model){
+		var modelDate = {
+			"name": model.get('name'),
+			"type": model.get('type'),
+			"src": model.get('src'),
+			"weburl": model.get('weburl'),
 		}
+		$('#webworkBlock .worksArea').append(workTmp(modelDate));
+	});	
+	illust.forEach(function(model){
+		var modelDate = {
+			"name": model.get('name'),
+			"type": model.get('type'),
+			"src": model.get('src'),
+		}
+		$('#illuworkBlock .worksArea').append(workTmp(modelDate));
+		if($('#illuworkBlock .worksArea .work').length > 0 && ($('#illuworkBlock .worksArea .work').length % 3) == 0){
+			$('#illuworkBlock .worksArea').append("<br />");
+		}
+	});
+
+
+
+
+	// Click Events
+  $('#contacts > div').on("click", function(e){
+  	var id = $(e.currentTarget).attr('id');
+  	var targetURL = '';
+  	if( id == 'fb'){
+  		targetURL = "https://www.facebook.com/FantasmaMeow";
+  	} else if(id == 'pixiv'){
+  		targetURL = "http://www.pixiv.net/member.php?id=303857";
+  	} else if( id == 'github'){
+  		targetURL = "https://github.com/FantasmaMeoW";
+  	}
+  
+  	window.open(targetURL,'_blank');
+  });
+  $('#illuworkBlock .worksArea .work').on('click', function(e){
+  	var name = $(e.currentTarget).attr('name');
+  	var model = Works.findWhere({'name':name}) ;
+  	var previewTmp = Handlebars.compile($('#previewTmp').html());
+  	var modelDate = {
+  		"src": model.get('src')
+  	};
+		$('#illuworkBlock .worksArea').after(previewTmp(modelDate));
+		var height = ($(window).height() - $('#preview img').height())/2
+		$('#preview img').css('margin-top', height);
+		$('#preview').css('opacity', '1');
 		
-	});
-	$('#menu .bg').on('click', function(){
-			$('#menu').removeClass('show');
-	});
-	$('#menu_list > li').on('click', function(e){
-		$('#menu_list .active').removeClass('active');
-		$(e.currentTarget).addClass('active');
-	});
+		$('.close').one('click', function(){
+  		$('#preview').fadeOut(function(){
+  			$('#preview').remove();
+  		});
+  		
+ 		});
+  });
+  
 });
-
-
 });
